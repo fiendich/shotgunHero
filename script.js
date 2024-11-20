39510e26da5cd17511626fba93dcbca1067d873b
fiendich <cmetanichel@gmail.com>
2024-11-20 16:44:34 +0100

    basic movement

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const ctx = canvas.getContext("2d");
 const CANVAS_WIDTH = canvas.width = 700; //set your canvas width...
 const CANVAS_HEIGHT = canvas.height = 860;
 
+const GRAVITY = 0.5
 class Player {
     constructor(position) {
         this.position = position
@@ -11,6 +12,7 @@ class Player {
             x: 0,
             y: 1,
         }
+        this.height = 100
     }
 
     draw () {
@@ -20,8 +22,15 @@ class Player {
 
     update() {
         this.draw()
-        this.position.y += this.velocity.y
-        this.velocity.y += 0.5
+        this.position.x += this.velocity.x
+        if (this.position.y + this.height + this.velocity.y < CANVAS_HEIGHT) {
+            this.velocity.y += GRAVITY
+        }
+        else {
+            this.velocity.y = 0
+        }
+            this.position.y += this.velocity.y
+        this.velocity.y += GRAVITY
     }
 }
 
@@ -34,7 +43,14 @@ const player2 = new Player({
     y: 100,
 })
 
-
+const keys = {
+    d: {
+        pressed: false,
+    },
+    a: {
+        pressed: false,
+    },
+}
 function animate() {
 
     window.requestAnimationFrame(animate)
@@ -42,10 +58,39 @@ function animate() {
     player.update()
     player2.update()
     
-    y ++ 
+    player.velocity.x = 0
+    if (keys.d.pressed) player.velocity.x = 5
+        else if (keys.a.pressed) player.velocity.x = -5
 }
 animate()
 
+window.addEventListener("keydown", (event) => {
+    switch (event.key) {
+        case "d":
+            keys.d.pressed = true
+            break
+
+        case "a":
+            keys.a.pressed = true
+            break    
+
+        case "w":
+            player.velocity.y = -20
+            break    
+    }    
+})
+
+window.addEventListener("keyup", (event) => {
+    switch (event.key) {
+        case "d":
+            keys.d.pressed = false
+            break
+
+        case "a":
+            keys.a.pressed = false
+            break 
+    }   
+})
 
 
 
@@ -61,29 +106,30 @@ animate()
 
 
 
-/*
-const spriteWidth = spriteHight = 256;
 
-const playerImage = new Image();
-playerImage.src = "assets/images/character_sheet_upscaled.png";
-let frameX = 0;
-let frameY = 6;
-let gameFrame = 0;
-const staggerFrames = 5;
 
-function animate() {
-    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHight, spriteWidth, spriteHight, 
-        0, 0, 160, 160);
+
+// const spriteWidth = spriteHight = 256;
+
+// const playerImage = new Image();
+// playerImage.src = "assets/images/character_sheet_upscaled.png";
+// let frameX = 0;
+// let frameY = 7;
+// let gameFrame = 0;
+// const staggerFrames = 7;
+
+// function animate() {
+//     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+//     ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHight, spriteWidth, spriteHight, 
+//         0, 0, 128, 128);
     
-    if (gameFrame % staggerFrames == 0) {
-        if (frameX < 9) frameX++;
-        else frameX = 0;
-    }
+//     if (gameFrame % staggerFrames == 0) {
+//         if (frameX < 5) frameX++;
+//         else frameX = 0;
+//     }
 
 
-    gameFrame++;
-    requestAnimationFrame(animate);
-}
-animate();
-*/
+//     gameFrame++;
+//     requestAnimationFrame(animate);
+// }
+// animate();
